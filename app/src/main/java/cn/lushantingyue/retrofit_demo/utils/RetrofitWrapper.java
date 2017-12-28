c2867b842cfcfe9a39ec9203dd8cce96a1c5b987
lushantingyue <diyikehan@126.com>
2017-12-28 18:00:50 +0800

    update

---
+++ app/src/main/java/cn/lushantingyue/retrofit_demo/utils/RetrofitWrapper.java
@@ -0,0 +1,11 @@
+package cn.lushantingyue.retrofit_demo.utils;
+
+/**
+ * Created by Lushantingyue on 2017/12/28 17.
+ * Responsibilities:
+ * Description:
+ * ProjectName:
+ */
+
+public class RetrofitWrapper {
+}
